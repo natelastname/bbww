9b4430f757e9c7fc7178541009676af1262c486b
Nate Nichols <nathannichols454@gmail.com>
2023-05-02 17:39:56 -0500

    Fixed typos

--- bbww.el
+++ bbww.el
@@ -31,6 +31,10 @@
 ;; `backward-kill-word', `forward-kill-word' that are generally less
 ;; greedy, especially when the backward region contains a newline.
 
+;; The fundamental purpose of bbww is to overhaul the default Emacs
+;; text editing functionality in a way that comes natural for users
+;; who are used to "normal" modern text editors.
+
 ;; See README.org for information on installation and configuration.
 
 ;;; Code:
